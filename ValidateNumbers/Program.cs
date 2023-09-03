173214f9c012bbb9511a9216b8fd258424e8cf54
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2023-09-03 13:51:28 -0300

    create the validade numbers program

--- ValidateNumbers/Program.cs
+++ ValidateNumbers/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Program {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número entre 1 e 10 \n");
+
+        var input = Console.ReadLine();
+
+        if (int.TryParse(input, out var number))
+        {
+            if (number < 0 || number > 10)
+            {
+                Console.WriteLine("Invalid input range.");
+                return;
+            }
+
+            Console.WriteLine("Valid input {0}", number);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input type.");
+            return;
+        }
+    }
+}
